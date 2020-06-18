e558dca2e89fe75fd107e5f3ab8233f75a81ff84
MinexAutomation <minex.automation@gmail.com>
2020-06-18 15:02:41 -0700

    Added extensions to handle cloning project- and package-references.

--- source/R5T.T0002/Code/Extensions/IVisualStudioProjectFileExtensions.cs
+++ source/R5T.T0002/Code/Extensions/IVisualStudioProjectFileExtensions.cs
@@ -6,6 +6,62 @@ namespace R5T.T0002
 {
     public static class IVisualStudioProjectFileExtensions
     {
+        /// <summary>
+        /// Method name indicates that we clone the package reference, instead of adding the actual instance.
+        /// </summary>
+        public static void AddNewPackageReferenceFrom(this IVisualStudioProjectFile visualStudioProjectFile, IPackageReference packageReference)
+        {
+            visualStudioProjectFile.AddPackageReference(packageReference.Name, packageReference.VersionString);
+        }
+
+        /// <summary>
+        /// Method name indicates that we clone the project reference, instead of adding the actual instance.
+        /// </summary>
+        public static void AddNewProjectReferenceFrom(this IVisualStudioProjectFile visualStudioProjectFile, IProjectReference projectReference)
+        {
+            visualStudioProjectFile.AddProjectReference(projectReference.ProjectFilePath);
+        }
+
+        public static void ClearPackageReferences(this IVisualStudioProjectFile visualStudioProjectFile)
+        {
+            var packageReferences = visualStudioProjectFile.PackageReferences.ToList();
+            foreach (var packageReference in packageReferences)
+            {
+                visualStudioProjectFile.RemovePackageReference(packageReference);
+            }
+        }
+
+        public static void ClearProjectReferences(this IVisualStudioProjectFile visualStudioProjectFile)
+        {
+            var projectReferences = visualStudioProjectFile.ProjectReferences.ToList();
+            foreach (var projectReference in projectReferences)
+            {
+                visualStudioProjectFile.RemoveProjectReference(projectReference);
+            }
+        }
+
+        public static void ClonePackageReferencesFrom(this IVisualStudioProjectFile visualStudioProjectFile, IVisualStudioProjectFile source)
+        {
+            visualStudioProjectFile.ClearPackageReferences();
+
+            var packageReferences = source.PackageReferences.ToList();
+            foreach (var packageReference in packageReferences)
+            {
+                visualStudioProjectFile.AddNewPackageReferenceFrom(packageReference);
+            }
+        }
+
+        public static void CloneProjectReferencesFrom(this IVisualStudioProjectFile visualStudioProjectFile, IVisualStudioProjectFile source)
+        {
+            visualStudioProjectFile.ClearProjectReferences();
+
+            var projectReferences = source.ProjectReferences.ToList();
+            foreach(var projectReference in projectReferences)
+            {
+                visualStudioProjectFile.AddNewProjectReferenceFrom(projectReference);
+            }
+        }
+
         public static IPackageReference GetPackageReference(this IVisualStudioProjectFile visualStudioProjectFile, string name, string versionString)
         {
             var hasPackageReference = visualStudioProjectFile.HasPackageReference(name, versionString, out var packageReference);
