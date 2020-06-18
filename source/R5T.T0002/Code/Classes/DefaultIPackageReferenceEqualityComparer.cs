33bdb37e27dc89298f6b3404467979c86c2293b0
MinexAutomation <minex.automation@gmail.com>
2020-06-18 15:03:22 -0700

    Added default package- and project-reference equality comparers.

---
+++ source/R5T.T0002/Code/Classes/DefaultIPackageReferenceEqualityComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.T0002
+{
+    public class DefaultIPackageReferenceEqualityComparer : IEqualityComparer<IPackageReference>
+    {
+        public static readonly DefaultIPackageReferenceEqualityComparer Instance = new DefaultIPackageReferenceEqualityComparer();
+
+
+        public bool Equals(IPackageReference x, IPackageReference y)
+        {
+            var areEqual = true;
+
+            var nameEquals = x.Name == y.Name;
+            areEqual &= nameEquals;
+
+            var versionStringEquals = x.VersionString == y.VersionString;
+            areEqual &= versionStringEquals;
+
+            return areEqual;
+        }
+
+        public int GetHashCode(IPackageReference obj)
+        {
+            var hashCode = obj.Name.GetHashCode();
+            return hashCode;
+        }
+    }
+}
---
+++ source/R5T.T0002/Code/Classes/DefaultProjectReferenceEqualityComparer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.T0002
+{
+    public class DefaultProjectReferenceEqualityComparer : IEqualityComparer<IProjectReference>
+    {
+        public static readonly DefaultProjectReferenceEqualityComparer Instance = new DefaultProjectReferenceEqualityComparer();
+
+
+        public bool Equals(IProjectReference x, IProjectReference y)
+        {
+            var areEqual = true;
+
+            var projectFilePathAreEqual = x.ProjectFilePath == y.ProjectFilePath;
+            areEqual &= projectFilePathAreEqual;
+
+            return areEqual;
+        }
+
+        public int GetHashCode(IProjectReference obj)
+        {
+            var hashCode = obj.ProjectFilePath.GetHashCode();
+            return hashCode;
+        }
+    }
+}
