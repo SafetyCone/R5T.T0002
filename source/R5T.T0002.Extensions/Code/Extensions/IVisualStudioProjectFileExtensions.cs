6c0e3692af343ab8945ea27891a3f3bb74b22a9b
David Coats <david@getsafetycone.com>
2020-08-02 11:36:22 -0700

    Added extensions library.

---
+++ source/R5T.T0002.Extensions/Code/Extensions/IVisualStudioProjectFileExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using R5T.D0029;
+
+
+namespace R5T.T0002
+{
+    public static class IVisualStudioProjectFileExtensions
+    {
+        public static List<string> GetDependencyProjectFilePaths(this IVisualStudioProjectFile visualStudioProjectFile)
+        {
+            var dependencyProjectFilePaths = visualStudioProjectFile.ProjectReferences
+                .Select(x => x.ProjectFilePath)
+                .ToList();
+
+            return dependencyProjectFilePaths;
+        }
+
+        public static async Task<List<string>> GetDependencyProjectFilePathsRecursive(this IVisualStudioProjectFile visualStudioProjectFile, IVisualStudioProjectFileSerializer visualStudioProjectFileSerializer)
+        {
+            var pathAccumulator = new HashSet<string>();
+
+            await visualStudioProjectFile.AccumulateDependencyProjectFilePathsRecursive(visualStudioProjectFileSerializer, pathAccumulator);
+
+            var dependencyProjectFilePaths = pathAccumulator
+                .OrderBy(x => x)
+                .ToList();
+
+            return dependencyProjectFilePaths;
+        }
+
+        private static async Task AccumulateDependencyProjectFilePathsRecursive(this IVisualStudioProjectFile visualStudioProjectFile, IVisualStudioProjectFileSerializer visualStudioProjectFileSerializer, HashSet<string> pathAccumulator)
+        {
+            var dependencyProjectFilePaths = visualStudioProjectFile.GetDependencyProjectFilePaths();
+
+            foreach (var projectFilePath in dependencyProjectFilePaths)
+            {
+                // Avoid deserializing the same project file multiple times by testing whether the project file has been visited or not.
+                if (!pathAccumulator.Contains(projectFilePath))
+                {
+                    pathAccumulator.Add(projectFilePath);
+
+                    var projectFile = await visualStudioProjectFileSerializer.DeserializeAsync(projectFilePath);
+
+                    await projectFile.AccumulateDependencyProjectFilePathsRecursive(visualStudioProjectFileSerializer, pathAccumulator);
+                }
+            }
+        }
+    }
+}
